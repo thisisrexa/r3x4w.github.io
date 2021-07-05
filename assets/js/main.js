daf1e67e8726d5d08a8dd29729a514884c9f580e
rexa <hi.r3x4w@gmail.com>
2021-07-05 12:56:09 +0430

    Changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,53 @@
-const ul = document.getElementById("links");
+let projects, links, des;
 
-function callAPI() {
-  let xhr = new XMLHttpRequest();
+projects = document.querySelector(".projects");
+links = document.querySelector(".links-container");
+
+function getProjects() {
+  const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      let response = JSON.parse(this.response);
-      response.map(function (item) {
-        links.innerHTML +=
-          "<li class='li'><a href=" +
-          item.url +
-          " class='link' target='_blank'>" +
-          item.name +
-          "</a></li>";
+      const sorted = JSON.parse(this.response);
+      const response = sorted.sort((a, b) => b.id - a.id);
+      response.map((project) => {
+        projects.innerHTML += `
+          <div class='card'>
+            <a href='${project.export}' target='_blank' class='card-image' style='background-image: url("${project.image}")'></a>
+            <section class='card-body'>
+              <span>${project.name}</span>
+            </section>
+          </div>
+        `;
       });
     }
   };
-  xhr.open("GET", "../../api.json", true);
+  xhr.open("GET", "../../projects.json", true);
   xhr.send();
 }
 
-document.addEventListener("DOMContentLoaded", callAPI());
+function getLinks() {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.response);
+      response.map((link) => {
+        if (link.description) des = link.description;
+        else des = "";
+        links.innerHTML += `
+          <li class='link-item'>
+            <a href='${links.href}' class='link'>${link.name}</a>
+            <span class='description'>${link.description}</span>
+          </li>
+        `;
+      });
+    }
+  };
+  xhr.open("GET", "../../links.json", true);
+  xhr.send();
+}
+
+document.addEventListener("DOMContentLoaded", getProjects());
+document.addEventListener("DOMContentLoaded", getLinks());
+
+// Debug
+// console.log(projects)
